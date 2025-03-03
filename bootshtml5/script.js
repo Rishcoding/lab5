387eb383701c8fd4525df7966e7dd666245c998e
Rishcoding <rpramanik@algomau.ca>
2025-03-02 23:20:09 -0500

    Add files via upload

---
+++ bootshtml5/script.js
@@ -0,0 +1,4 @@
+document.getElementById("theme-toggle").addEventListener("click", function () {
+    document.body.classList.toggle("bg-dark");
+    document.body.classList.toggle("text-white");
+});
